2fe78c89f9092f1242ec39473a62f4b835d7deb5
Ashikparvez89 <ashikparvez89@gmail.com>
2024-07-15 20:19:38 +0800

    added all

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://hyper-place.web.app",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hotel Booking I Here");
 });
-
-console.log(process.env.DB_USER, process.env.DB_PASS);
 
 // hotelServer
 
@@ -32,6 +41,25 @@ const client = new MongoClient(uri, {
   },
 });
 
+// jwt b=verify middleware
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unaothorised access" });
+  }
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+      if (err) {
+        console.log("not verify");
+        return res.status(401).send({ message: "Unaothorised access" });
+      }
+      req.user = decoded;
+      next();
+    });
+  }
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -41,6 +69,33 @@ async function run() {
 
     const jobsCollection = client.db("Hyper_place").collection("jobs");
     const bidsCollection = client.db("Hyper_place").collection("bids");
+
+    // genarete token from browser while login
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
 
     // Get all jobs route
 
@@ -53,6 +108,59 @@ async function run() {
     app.get("/alljobs", async (req, res) => {
       const result = await jobsCollection.find().toArray();
       res.send(result);
+    });
+
+    // get all job for pagination
+
+    app.get("/all-jobs", async (req, res) => {
+      const size = parseInt(req.query.size) - 1;
+      const page = parseInt(req.query.page);
+      const filter = req.query.filter;
+      const sort = req.query.sort;
+      const search = req.query.search;
+      console.log(search);
+
+      let options = {};
+      if (sort) {
+        const sortField = "deadline";
+        const sortOrder = sort === "asc" ? 1 : -1;
+        options.sort = { [sortField]: sortOrder };
+      }
+
+      let query = {};
+      if (search) {
+        query.jobName = { $regex: search, $options: "i" };
+      }
+      if (filter && filter !== "All") {
+        query.category = filter;
+      }
+
+      // if (filter && filter !== "All") {
+      //   query = { category: filter };
+      // }
+      const result = await jobsCollection
+        .find(query)
+        .skip(size * page)
+        .limit(page)
+        .sort(options.sort || {})
+        .toArray();
+      res.send(result);
+    });
+
+    // get all jobs number for pagination
+
+    app.get("/alljobs-count", async (req, res) => {
+      const filter = req.query.filter;
+      const search = req.query.search;
+      let query = {};
+      if (search) {
+        query.jobName = { $regex: search, $options: "i" };
+      }
+      if (filter && filter !== "All") {
+        query.category = filter;
+      }
+      const count = await jobsCollection.countDocuments(query);
+      res.send({ count });
     });
 
     app.put("/alljobs/:id", async (req, res) => {
@@ -69,15 +177,14 @@ async function run() {
       res.send(result);
     });
 
-    app.delete(`/alljobs/:id`, async (req, res) => {
+    app.delete(`/alljobs/:id`, verifyToken, async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.get("/myjobs/:email", async (req, res) => {
+    app.get("/myjobs/:email", verifyToken, async (req, res) => {
       const email = req.params?.email;
       const query = { authorEmail: email };
       const result = await jobsCollection.find(query).toArray();
@@ -85,16 +192,57 @@ async function run() {
       return;
     });
 
-    app.post("/bids", async (req, res) => {
+    app.post("/bids", verifyToken, async (req, res) => {
       const bidData = req.body;
+      console.log(bidData);
+      // check if data exits on db
+
+      const query = { userMail: bidData?.userMail, jobid: bidData.jobid };
+      console.log("query email", query);
+      const appliedJob = await bidsCollection.findOne(query);
+      console.log("applied mail", appliedJob);
+      if (appliedJob) {
+        return res.status(400).send({
+          message:
+            "You already bid for this job, please try onother one job for bid",
+        });
+      }
       const result = await bidsCollection.insertOne(bidData);
       res.send(result);
     });
 
-    app.get("/mybids/:email", async (req, res) => {
+    app.get("/mybids/:email", verifyToken, async (req, res) => {
+      const tokenEmail = req.user.email;
+      console.log(tokenEmail);
       const email = req.params.email;
+      if (tokenEmail !== email) {
+        return res.status(401).send({ message: "unaothourised access" });
+      }
       const query = { userMail: email };
       const result = await bidsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/bidreq/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { authourEmail: email };
+      const result = await bidsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/mybids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { jobid: id };
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/bidreq/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateData = {
+        $set: status,
+      };
+      const result = await bidsCollection.updateOne(query, updateData);
       res.send(result);
     });
 
